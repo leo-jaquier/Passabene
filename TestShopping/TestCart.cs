eff4b4298e1e006edc271d81f779dcc7c046cb83
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-10-06 16:27:56 +0200

    test: add test case for adding items in cart

--- TestShopping/TestCart.cs
+++ TestShopping/TestCart.cs
@@ -19,8 +19,8 @@ namespace TestShopping
         {
             //given
             //refer to Setup
-            float articlePrice = 4.5f;
-            Article expectedArticle = new Article(articlePrice);
+            float expectedArticlePrice = 4.5f;
+            Article expectedArticle = new Article(expectedArticlePrice);
             int expectedQuantity = 1;
             CartItem expectedCartItem = new CartItem(expectedArticle, expectedQuantity);
             List<CartItem> expectedCartItems = new List<CartItem> { expectedCartItem };
@@ -30,8 +30,8 @@ namespace TestShopping
             _cart.Add(expectedCartItems);
 
             //then
-            Assert.That(_cart.CartItems.Count(), Is.EqualTo(1));
-            Assert.That(_cart.Price, Is.EqualTo(articlePrice));
+            Assert.That(_cart.CartItems.Count(), Is.EqualTo(expectedQuantity));
+            Assert.That(_cart.Price, Is.EqualTo(expectedArticlePrice));
         }
 
         [Test]
@@ -40,13 +40,13 @@ namespace TestShopping
             //given
             //refer to Setup
             //TODO must be generated automatically
-            float articlePrice1 = 4.5f;
-            Article expectedArticle1 = new Article(articlePrice1);
+            float expectedArticlePrice1 = 4.5f;
+            Article expectedArticle1 = new Article(expectedArticlePrice1);
             int expectedQuantity1 = 1;
             CartItem expectedCartItem1 = new CartItem(expectedArticle1, expectedQuantity1);
 
-            float articlePrice2 = 5.0f;
-            Article expectedArticle2 = new Article(articlePrice2);
+            float expectedArticlePrice2 = 5.0f;
+            Article expectedArticle2 = new Article(expectedArticlePrice2);
             int expectedQuantity2 = 1;
             CartItem expectedCartItem2 = new CartItem(expectedArticle2, expectedQuantity2);
 
@@ -57,8 +57,30 @@ namespace TestShopping
             _cart.Add(expectedCartItems);
 
             //then
-            Assert.That(_cart.CartItems.Count(), Is.EqualTo(1));
-            Assert.That(_cart.Price, Is.EqualTo(articlePrice1 + articlePrice2));
+            Assert.That(_cart.CartItems.Count(), Is.EqualTo(expectedQuantity1 + expectedQuantity2));
+            Assert.That(_cart.Price, Is.EqualTo(expectedArticlePrice1 + expectedArticlePrice2));
+        }
+
+        [Test]
+        public void Add_OneMultipleCartItems_Success()
+        {
+            //given
+            //refer to Setup
+            //TODO must be generated automatically
+            float expectedArticlePriced = 4.5f;
+            Article expectedArticle = new Article(expectedArticlePriced);
+            int expectedQuantity = 2;
+            CartItem expectedCartItem1 = new CartItem(expectedArticle, expectedQuantity);
+
+            List<CartItem> expectedCartItems = new List<CartItem> { expectedCartItem };
+            Assert.That(_cart.CartItems.Count(), Is.EqualTo(0));
+
+            //when
+            _cart.Add(expectedCartItems);
+
+            //then
+            Assert.That(_cart.CartItems.Count(), Is.EqualTo(expectedQuantity));
+            Assert.That(_cart.Price, Is.EqualTo(expectedArticlePriced * expectedQuantity));
         }
     }
 }
